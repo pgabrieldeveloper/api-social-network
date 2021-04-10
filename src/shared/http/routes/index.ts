ad88d6aaf32ef8c3470aeb034f30862a7db28440
pgabrieldeveloper <g4brieldev@gmail.com>
2021-04-10 10:44:06 -0300

    migration user

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,7 @@
 import { Router } from 'express';
-
 const routes = Router();
 routes.get('/', (req, res) => {
-  return res.json({ message: 'Olá express' });
+  return res.json({ message: 'Olá api-social-network' });
 });
 
 export default routes;
---
+++ src/shared/typeorm/migrations/1618061316552-UserMigration.ts
@@ -0,0 +1,42 @@
+import { query } from 'express';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class UserMigration1618061316552 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'user',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+          },
+          {
+            name: 'avatar',
+            type: 'varchar',
+            isNullable: true,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('user');
+  }
+}
