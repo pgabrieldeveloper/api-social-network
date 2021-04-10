fd4bb8f8606ddd92af71b9e77ce733f9096b435e
pgabrieldeveloper <g4brieldev@gmail.com>
2021-04-10 12:28:11 -0300

    controller Create User

--- src/modules/user/controllers/UserController.ts
+++ src/modules/user/controllers/UserController.ts
@@ -1,3 +1,11 @@
-class UserController {}
+import { Request, Response } from 'express';
+import CreateUserService from '../services/CreateUserService';
+class UserController {
+  public async create(req: Request, res: Response) {
+    const { name, email, password } = req.body;
+    const user = await CreateUserService.execute({ name, email, password });
+    return res.status(201).json(user);
+  }
+}
 
 export default new UserController();
