d4996461d7eb6d19a874911f8cd79e71b22752dc
pgabrieldeveloper <g4brieldev@gmail.com>
2021-04-10 12:32:23 -0300

    userRouter

--- src/modules/user/routes/user.routes.ts
+++ src/modules/user/routes/user.routes.ts
@@ -2,7 +2,5 @@ import { Router } from 'express';
 import UserController from '../controllers/UserController';
 
 const userRouter = Router();
-
 userRouter.post('/', UserController.create);
-
 export default userRouter;
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
+import userRouter from '../../../modules/user/routes/user.routes';
 const routes = Router();
 routes.get('/', (req, res) => {
   return res.json({ message: 'Olá api-social-network' });
 });
+routes.use('/user', userRouter);
 
 export default routes;
