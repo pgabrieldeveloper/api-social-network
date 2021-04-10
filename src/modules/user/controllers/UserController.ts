ee2852b4413c7635374b6e8caca1cd4bcff8cfc3
pgabrieldeveloper <g4brieldev@gmail.com>
2021-04-10 14:25:05 -0300

    UserController Login

--- src/modules/user/controllers/UserController.ts
+++ src/modules/user/controllers/UserController.ts
@@ -1,10 +1,16 @@
 import { Request, Response } from 'express';
 import CreateUserService from '../services/CreateUserService';
+import LoginUserService from '../services/LoginUserService';
 class UserController {
   public async create(req: Request, res: Response) {
     const { name, email, password } = req.body;
     const user = await CreateUserService.execute({ name, email, password });
     return res.status(201).json(user);
+  }
+  public async login(req: Request, res: Response) {
+    const { email, password } = req.body;
+    const userToken = await LoginUserService.execute({ email, password });
+    return res.status(200).json(userToken);
   }
 }
 
