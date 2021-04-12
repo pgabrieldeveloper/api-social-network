f0ad502c5cca9e5f70a1bf2db2384c5f50cabfdf
pgabrieldeveloper <g4brieldev@gmail.com>
2021-04-12 10:20:36 -0300

    userRoutes patch upload avatar

--- src/modules/user/routes/user.routes.ts
+++ src/modules/user/routes/user.routes.ts
@@ -1,7 +1,19 @@
 import { Router } from 'express';
 import UserController from '../controllers/UserController';
+import multer from 'multer';
+import uploadConfigAvatar from '../../../config/uploadAvatar';
+import isAuthenticate from '../middlewares/isAuthenticate';
 
 const userRouter = Router();
+
+const upload = multer(uploadConfigAvatar);
+
 userRouter.post('/', UserController.create);
 userRouter.post('/login', UserController.login);
+userRouter.patch(
+  '/avatar',
+  isAuthenticate,
+  upload.single('avatar'),
+  UserController.uploadAvatar,
+);
 export default userRouter;
